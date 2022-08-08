5019515dee1971ebc3b12e7492509dbc959c2a13
Вячеслав <vatheslav-91@mail.ru>
2022-08-08 18:06:32 +0500

    ДЗ массивы ч2(исправил ошибки)

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-int[] arr = generateRandomArray();
-int sum = 0;
-for (int zatratyDay : arr){
-    sum += zatratyDay;
-}
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int expensesDay : arr) {
+            sum += expensesDay;
+        }
         System.out.printf("Сумма трат за месяц составила %s рублей\n", sum);
 
-int minZatratyDay = arr[0];
-        int maxZatratyDay = arr[0];
-        for (int i = 1; i < arr.length; i++){
-            if (minZatratyDay > arr[i]) {
-                minZatratyDay = arr[i];
-            } else if (maxZatratyDay < arr[i]) {
-                maxZatratyDay = arr[i];
+        int minExpensesDay = arr[0];
+        int maxExpensesDay = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (minExpensesDay > arr[i]) {
+                minExpensesDay = arr[i];
+            } else if (maxExpensesDay < arr[i]) {
+                maxExpensesDay = arr[i];
             }
         }
-        System.out.printf("Минимальная сумма трат за день составила %s рублей\n"
-                + "Максимальная сумма трат за день составила %s рублей\n", minZatratyDay , maxZatratyDay);
+        System.out.printf("Минимальная сумма трат за день составила %s рублей\n" + "Максимальная сумма трат за день составила %s рублей\n", minExpensesDay, maxExpensesDay);
 
 
-
-        double zredniechislo =(double) sum/ arr.length;
-        System.out.printf("Средняя сумма трат за месяц составила %s рублей\n", zredniechislo);
-
+        double averageExpenses = (double) sum / arr.length;
+        System.out.printf("Средняя сумма трат за месяц составила %s рублей\n", averageExpenses);
 
 
-
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = reverseFullName.length - 1; i >= 0 ; i--) {
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
             System.out.print(reverseFullName[i]);
         }
 
-        }
-
-
-
+    }
 
 
     int[] arr = generateRandomArray();
